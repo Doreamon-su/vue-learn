35fbe7c3ea34079a768ef887ef75948108759f9e
sux26 <sux26@chinaunicom.cn>
2020-08-20 14:27:32 +0800

    实现简单的图片验证

--- src/components/SliderConfirm.vue
+++ src/components/SliderConfirm.vue
@@ -1,23 +1,30 @@
 <template>
   <div id="slider-confirm">
-    <!-- 绿色背景 -->
-    <div id="dragBg" ref="dragBg"></div>
-    <!-- 滑动容器文本 -->
-    <div id="dragText">{{result}}</div>
-    <!-- 滑块 -->
-    <div id="dragHandler" @mousedown="drag" ref="dragHandler">{{count}}</div>
+    <div id="slider-img">
+      <img src="../assets/confirm.png" alt id="img" />
+      <img src="../assets/patch.png" alt id="patch" />
+    </div>
+    <div id="slider-bar">
+      <!-- 绿色背景 -->
+      <div id="dragBg" ref="dragBg"></div>
+      <!-- 滑动容器文本 -->
+      <div id="dragText" v-if="isPass">验证通过</div>
+      <!-- 滑块 -->
+      <div id="dragHandler" @mousedown="drag">{{count}}</div>
+    </div>
   </div>
 </template>
 <script>
 export default {
   data() {
     return {
-      count: 0,
-      result: "滑动验证",
+      count: 0, //显示百分比
+      isPass: false, //是否通过验证
     };
   },
   methods: {
     drag: function () {
+      var patch = document.querySelector("#patch");
       var dragHandler = document.querySelector("#dragHandler");
       var fixX = event.clientX - dragHandler.offsetLeft;
       var self = this;
@@ -25,17 +32,24 @@ export default {
         var distanceX = event.clientX - fixX;
         if (distanceX < 0) {
           distanceX = 0;
-        } else if (distanceX > 260) {
-          distanceX = 260;
+        } else if (distanceX > 300) {
+          distanceX = 300;
         }
         dragHandler.style.left = distanceX + "px";
+        patch.style.left = distanceX + "px";
         document.querySelector("#dragBg").style.width = distanceX + "px";
-        self._data.count = parseInt((distanceX / 260) * 100);
-        if (self._data.count == 100) {
-          self._data.result = "验证通过";
-        }
+        self._data.count = parseInt((distanceX / 300) * 100);
       };
       document.onmouseup = function () {
+        if (self._data.count == 72) {
+          self._data.isPass = true;
+        } else {
+          self._data.isPass = false;
+          self._data.count = 0;
+          dragHandler.style.left = 0 + "px";
+          patch.style.left = 0 + "px";
+          document.querySelector("#dragBg").style.width = 0 + "px";
+        }
         document.onmousemove = null;
         document.onmouseup = null;
       };
@@ -44,17 +58,32 @@ export default {
 };
 </script>
 <style>
-#slider-confirm {
+#slider-img {
+  position: relative;
+}
+#img {
+  width: 330px;
+  position: relative;
+  left: -2px;
+}
+#patch {
+  position: absolute;
+  border: 1px solid black;
+  left: 0px;
+  top: 72.6px;
+  width: 33px;
+}
+#slider-bar {
   position: relative;
   display: block;
   background: #e8e8e8;
-  width: 300px;
-  height: 33px;
+  width: 330px;
+  height: 30px;
   border: 2px solid #e8e8e8;
 }
 #dragBg {
   position: absolute;
-  background-color: #7ac23c;
+  background-color: #00aeff;
   width: 0px;
   height: 100%;
 }
@@ -74,7 +103,7 @@ export default {
 /* 滑块 */
 #dragHandler {
   position: absolute;
-  width: 40px;
+  width: 30px;
   height: 100%;
   cursor: move;
   background: #fff;
